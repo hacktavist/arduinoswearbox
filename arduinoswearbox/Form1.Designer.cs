ba81658c067af2b816592aee1fa8f7c1431ebb01
hacktavist <hacktavist616@gmail.com>
2018-10-31 09:13:34 -0400

    initial commit for money making swear box scheme mwuahahah

---
+++ arduinoswearbox/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace arduinoswearbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.arduinoComPortCombo = new System.Windows.Forms.ComboBox();
+            this.arduinoComPortLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.serialPortConnection = new System.Windows.Forms.Button();
+            this.speechRecognitionOutputTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // arduinoComPortCombo
+            // 
+            this.arduinoComPortCombo.BackColor = System.Drawing.Color.Black;
+            this.arduinoComPortCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.arduinoComPortCombo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.arduinoComPortCombo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.arduinoComPortCombo.ForeColor = System.Drawing.Color.Lime;
+            this.arduinoComPortCombo.FormattingEnabled = true;
+            this.arduinoComPortCombo.Location = new System.Drawing.Point(168, 73);
+            this.arduinoComPortCombo.Name = "arduinoComPortCombo";
+            this.arduinoComPortCombo.Size = new System.Drawing.Size(121, 28);
+            this.arduinoComPortCombo.TabIndex = 0;
+            // 
+            // arduinoComPortLabel
+            // 
+            this.arduinoComPortLabel.AutoSize = true;
+            this.arduinoComPortLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.arduinoComPortLabel.Location = new System.Drawing.Point(12, 76);
+            this.arduinoComPortLabel.Name = "arduinoComPortLabel";
+            this.arduinoComPortLabel.Size = new System.Drawing.Size(150, 20);
+            this.arduinoComPortLabel.TabIndex = 1;
+            this.arduinoComPortLabel.Text = "Arduino Com Port";
+            // 
+            // exitButton
+            // 
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.ForeColor = System.Drawing.Color.Red;
+            this.exitButton.Location = new System.Drawing.Point(883, 484);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // serialPortConnection
+            // 
+            this.serialPortConnection.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.serialPortConnection.Location = new System.Drawing.Point(311, 76);
+            this.serialPortConnection.Name = "serialPortConnection";
+            this.serialPortConnection.Size = new System.Drawing.Size(75, 23);
+            this.serialPortConnection.TabIndex = 3;
+            this.serialPortConnection.Text = "Connect";
+            this.serialPortConnection.UseVisualStyleBackColor = true;
+            this.serialPortConnection.Click += new System.EventHandler(this.serialPortConnection_Click);
+            // 
+            // speechRecognitionOutputTextbox
+            // 
+            this.speechRecognitionOutputTextbox.AcceptsReturn = true;
+            this.speechRecognitionOutputTextbox.BackColor = System.Drawing.Color.Black;
+            this.speechRecognitionOutputTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.speechRecognitionOutputTextbox.ForeColor = System.Drawing.Color.Lime;
+            this.speechRecognitionOutputTextbox.Location = new System.Drawing.Point(452, 73);
+            this.speechRecognitionOutputTextbox.Multiline = true;
+            this.speechRecognitionOutputTextbox.Name = "speechRecognitionOutputTextbox";
+            this.speechRecognitionOutputTextbox.ReadOnly = true;
+            this.speechRecognitionOutputTextbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.speechRecognitionOutputTextbox.Size = new System.Drawing.Size(462, 178);
+            this.speechRecognitionOutputTextbox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(974, 527);
+            this.ControlBox = false;
+            this.Controls.Add(this.speechRecognitionOutputTextbox);
+            this.Controls.Add(this.serialPortConnection);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.arduinoComPortLabel);
+            this.Controls.Add(this.arduinoComPortCombo);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Arduino Swear Box";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox arduinoComPortCombo;
+        private System.Windows.Forms.Label arduinoComPortLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button serialPortConnection;
+        private EventHandler connectSerial_Click;
+        private System.Windows.Forms.TextBox speechRecognitionOutputTextbox;
+    }
+}
+
---
+++ arduinoswearbox/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Speech.Recognition;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace arduinoswearbox
+{
+
+    public partial class Form1 : Form
+    {
+        // Speech Recognition setup
+        SpeechRecognitionEngine speechRE = null;
+        List<Word> words = new List<Word>();
+
+        // Arduino/COM setup
+        SerialPort port;
+        String[] ports;
+        bool isConnected = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Speech
+            speechRE = new SpeechRecognitionEngine();
+            speechRE.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(engine_SpeechRecognized);
+            speechRE.SpeechRecognitionRejected += new EventHandler<SpeechRecognitionRejectedEventArgs>(engine_SpeechRejected);
+            loadGrammarAndCommands();
+            speechRE.SetInputToDefaultAudioDevice();
+            speechRE.RecognizeAsync(RecognizeMode.Multiple);
+
+            // Arduino/COM 
+            getAvailableComPorts();
+            foreach (string port in ports)
+            {
+                arduinoComPortCombo.Items.Add(port);
+                Console.WriteLine(port);
+                if (ports[0] != null)
+                {
+                    arduinoComPortCombo.SelectedItem = ports[0];
+                }
+            }
+        }
+
+        private void engine_SpeechRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            sendToArduino("#FINE\n");
+            speechRecognitionOutputTextbox.AppendText("#FINE");
+            speechRecognitionOutputTextbox.AppendText(Environment.NewLine);
+
+        }
+
+        private void engine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            speechRecognitionOutputTextbox.AppendText(getKnownTextOrExecute(e.Result.Text));
+            speechRecognitionOutputTextbox.AppendText(Environment.NewLine);
+
+        }
+
+        private string getKnownTextOrExecute(string text)
+        {
+            try
+            {
+                var cmd = words.Where(c => c.Text == text).First();
+
+                if (cmd.IsShellCommand)
+                {
+                    Process proc = new Process();
+                    proc.EnableRaisingEvents = false;
+                    proc.StartInfo.FileName = cmd.AttachedText;
+                    proc.Start();
+                    return "you just started: " + cmd.AttachedText;
+                }
+                else
+                {
+                    sendToArduino(cmd.AttachedText);
+                    return cmd.AttachedText;
+                }
+            }
+            catch (Exception)
+            {
+                return text;
+            }
+        }
+
+        private void sendToArduino(String cmd)
+        {
+            if (isConnected)
+            {
+                port.Write(cmd + "\n");
+            }
+        }
+
+        private void loadGrammarAndCommands()
+        {
+            try
+            {
+                Choices texts = new Choices();
+                string[] lines = File.ReadAllLines(Environment.CurrentDirectory + "\\ForbiddenWords.txt");
+                foreach (string line in lines)
+                {
+                    // skip commentblocks and empty lines..
+                    if (line.StartsWith("--") || line == String.Empty) continue;
+
+                    // split the line
+                    var parts = line.Split(new char[] { '|' });
+
+                    // add commandItem to the list for later lookup or execution
+                    words.Add(new Word() { Text = parts[0], AttachedText = parts[1], IsShellCommand = (parts[2] == "true") });
+
+                    // add the text to the known choices of speechengine
+                    texts.Add(parts[0]);
+                }
+                Grammar wordsList = new Grammar(new GrammarBuilder(texts));
+                speechRE.LoadGrammar(wordsList);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        void getAvailableComPorts()
+        {
+            ports = SerialPort.GetPortNames();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        void arduinoConnect()
+        {
+            isConnected = true;
+            string selectedPort = arduinoComPortCombo.GetItemText(arduinoComPortCombo.SelectedItem);
+            port = new SerialPort(selectedPort, 9600, Parity.None, 8, StopBits.One);
+            port.Open();
+            port.Write("#STAR\n");
+            serialPortConnection.ForeColor = Color.Red;
+            serialPortConnection.Text = "Disconnect";
+
+        }
+
+        void arduinoDisconnect()
+        {
+            isConnected = false;
+            port.Write("#STOP\n");
+            port.Close();
+            serialPortConnection.ForeColor = Color.Lime;
+            serialPortConnection.Text = "Connect";
+        }
+
+        private void serialPortConnection_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                arduinoConnect();
+            }
+            else
+            {
+                arduinoDisconnect();
+            }
+        }
+    }
+
+
+
+    public class Word
+    {
+        public Word() { }
+        public string Text { get; set; }
+        public string AttachedText { get; set; }
+        public bool IsShellCommand { get; set; }
+    }
+
+}
