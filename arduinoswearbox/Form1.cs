1375148224ebf84f246ca22a81bcd4feee63137f
hacktavist <hacktavist616@gmail.com>
2018-11-07 01:59:45 -0500

    cleaning up old SAPI shit to make room for google cloud api

--- arduinoswearbox/Form1.cs
+++ arduinoswearbox/Form1.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Drawing;
-using System.Windows;
 using System.Windows.Forms;
 using System.IO.Ports;
-using System.Speech.Recognition;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
 
 namespace arduinoswearbox
 {
@@ -15,8 +9,7 @@ namespace arduinoswearbox
     public partial class Form1 : Form
     {
         // Speech Recognition setup
-        SpeechRecognitionEngine speechRE = null;
-        List<Word> words = new List<Word>();
+
 
         // Arduino/COM setup
         SerialPort port;
@@ -27,12 +20,7 @@ namespace arduinoswearbox
             InitializeComponent();
 
             // Speech
-            speechRE = new SpeechRecognitionEngine();
-            speechRE.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(engine_SpeechRecognized);
-            speechRE.SpeechRecognitionRejected += new EventHandler<SpeechRecognitionRejectedEventArgs>(engine_SpeechRejected);
-            loadGrammarAndCommands();
-            speechRE.SetInputToDefaultAudioDevice();
-            speechRE.RecognizeAsync(RecognizeMode.Multiple);
+
 
             // Arduino/COM 
             getAvailableComPorts();
@@ -47,81 +35,11 @@ namespace arduinoswearbox
             }
         }
 
-        private void engine_SpeechRejected(object sender, SpeechRecognitionRejectedEventArgs e)
-        {
-            sendToArduino("#FINE\n");
-            speechRecognitionOutputTextbox.AppendText("#FINE");
-            speechRecognitionOutputTextbox.AppendText(Environment.NewLine);
-
-        }
-
-        private void engine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
-        {
-            speechRecognitionOutputTextbox.AppendText(getKnownTextOrExecute(e.Result.Text));
-            speechRecognitionOutputTextbox.AppendText(Environment.NewLine);
-
-        }
-
-        private string getKnownTextOrExecute(string text)
-        {
-            try
-            {
-                var cmd = words.Where(c => c.Text == text).First();
-
-                if (cmd.IsShellCommand)
-                {
-                    Process proc = new Process();
-                    proc.EnableRaisingEvents = false;
-                    proc.StartInfo.FileName = cmd.AttachedText;
-                    proc.Start();
-                    return "you just started: " + cmd.AttachedText;
-                }
-                else
-                {
-                    sendToArduino(cmd.AttachedText);
-                    return cmd.AttachedText;
-                }
-            }
-            catch (Exception)
-            {
-                return text;
-            }
-        }
-
         private void sendToArduino(String cmd)
         {
             if (isConnected)
             {
                 port.Write(cmd + "\n");
-            }
-        }
-
-        private void loadGrammarAndCommands()
-        {
-            try
-            {
-                Choices texts = new Choices();
-                string[] lines = File.ReadAllLines(Environment.CurrentDirectory + "\\ForbiddenWords.txt");
-                foreach (string line in lines)
-                {
-                    // skip commentblocks and empty lines..
-                    if (line.StartsWith("--") || line == String.Empty) continue;
-
-                    // split the line
-                    var parts = line.Split(new char[] { '|' });
-
-                    // add commandItem to the list for later lookup or execution
-                    words.Add(new Word() { Text = parts[0], AttachedText = parts[1], IsShellCommand = (parts[2] == "true") });
-
-                    // add the text to the known choices of speechengine
-                    texts.Add(parts[0]);
-                }
-                Grammar wordsList = new Grammar(new GrammarBuilder(texts));
-                speechRE.LoadGrammar(wordsList);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
             }
         }
 
@@ -168,16 +86,6 @@ namespace arduinoswearbox
                 arduinoDisconnect();
             }
         }
-    }
-
-
-
-    public class Word
-    {
-        public Word() { }
-        public string Text { get; set; }
-        public string AttachedText { get; set; }
-        public bool IsShellCommand { get; set; }
     }
 
 }
