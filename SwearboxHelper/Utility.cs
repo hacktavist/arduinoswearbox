ddba5b92a76544c457f15b6e8e1fce2afc9f58e3
hacktavist <hacktavist616@gmail.com>
2018-11-07 02:55:42 -0500

    made a helper library to do speech shit

---
+++ SwearboxHelper/Utility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace SwearboxHelper
+{
+    class Utility
+    {
+        public static readonly string ForbiddenWords = "ForbiddenWords.txt";
+
+        public static bool CheckForbiddenWords(string sentence)
+        {
+            foreach (var swear in File.ReadAllLines(ForbiddenWords).ToList())
+                foreach (var word in sentence.Split(' '))
+                    if (word == swear) return true;
+            return false;
+        }
+    }
+}
